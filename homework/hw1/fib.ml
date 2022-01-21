e03b41ca37913283df0f574e0b56a412d6a94fed
JessicaZng <zhiyun_zhang@ucsb.edu>
2022-01-20 17:54:48 -0800

    hw1 IP

--- homework/hw1/fib.ml
+++ homework/hw1/fib.ml
@@ -1,3 +1,12 @@
 (** Computes the n-th Fibonacci number. *)
 let rec fib (n: int) : int =
-  failwith "TODO: homework"
+  match n with
+    0 -> 0
+    | 1 -> 1
+    | _ -> fib (n-1) + fib (n-2)
+
+  (* another valid method:   
+    if n = 0 then 0
+    else if n = 1 then 1
+    else fib(n-1) + fib(n-2)
+  *)
--- homework/hw1/max.ml
+++ homework/hw1/max.ml
@@ -2,4 +2,18 @@
   * or None otherwise.
   *)
 let rec max (ns: int list) : int option =
-  failwith "TODO: homework"
+  match ns with
+  [] -> None
+  | (h::t) -> (
+    match t with
+    [] -> Some h
+    | (hd::tl) -> if (h > hd) then (max (h::tl)) else (max (hd::tl))
+  )
+      
+    (* let rec max_helper h t = 
+    match t with
+    [] -> Some h
+    | (hd::tl) -> if (h > hd) then (max_helper h tl) else (max_helper hd tl);; *)
+
+    (* For this problem, i referenced this website for the idea: 
+https://stackoverflow.com/questions/58157626/finding-max-min-number-in-list-in-ocaml *)
